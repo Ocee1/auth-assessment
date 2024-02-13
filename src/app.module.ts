1b6f54bb062b7712292aa9454a30d7e8abffd7e7
Ocee <ocjay24@gmail.com>
2024-02-13 21:35:28 +0100

    update to the migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,26 +6,24 @@ import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CustomInterceptor } from './interceptor';
 
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      
       driver: ApolloDriver,
-      cors: {
-        origin: true,
-        credentials: true,
-      },
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
     }),
-    
     AuthModule,
     UserModule,
   ],
   controllers: [],
-  providers: [PrismaService],
+  providers: [PrismaService, {provide: APP_INTERCEPTOR, useClass: CustomInterceptor}],
 })
 export class AppModule {}
