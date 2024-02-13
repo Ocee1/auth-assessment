ee76ba11008ba0f9aad25d5f93cee9908dc4c96f
Ocee <ocjay24@gmail.com>
2024-02-13 19:43:54 +0100

    added cors config

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// import * as cors from '@nestjs/co';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); 
-  app.enableCors({
-    origin: '*',
-    credentials: true,
-  })
+  app.enableCors();
+  // app.use((cors<cors.CorsRequest>({
+  //   origin: 'https://auth-api-wn9m.onrender.com/',
+  //   credentials: true,
+  // })));
   await app.listen(3000);
 }
 bootstrap();
