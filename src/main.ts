c5b7217918d8d7b4500671e406ea1a4b946ed431
Ocee <ocjay24@gmail.com>
2024-02-13 19:18:17 +0100

    update to the migrations

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import cors from 'cors';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); 
-  app.use(cors({ origin: '*' }));
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  })
   await app.listen(3000);
 }
 bootstrap();
